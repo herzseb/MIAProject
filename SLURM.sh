df0b899dc408a5438a5834961a0f0f6fdfe4503a
herzseb <herzbasti97@gmail.co>
2023-05-19 22:03:39 +0200

    params

--- SLURM.sh
+++ SLURM.sh
@@ -6,12 +6,11 @@
 #SBATCH --partition=long
 #SBATCH --qos=users        
 #SBATCH --account=users    
-#SBATCH --gres=gpu:tesla_V100:1
+#SBATCH --gres=gpu:tesla_v100:1
 #SBATCH --time=7:0:0        
 #SBATCH --output=test-%j.out    
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sherz22@ku.edu.tr    
-module load python/3.7.4
+module load python/3.9.5
 module load cuda/11.4
-module load cudnn/8.2.2/cuda-11.4
 python main.py
